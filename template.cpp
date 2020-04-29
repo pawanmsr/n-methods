4acf5a54b08b1df8eb816046a19d3ab776878569
Pawan Harendra Mishra <pawanharendramishra@Pawan.local>
2020-04-29 16:22:15 +0530

    add template

---
+++ template.cpp
@@ -0,0 +1,71 @@
+/* 
+ * Author: awesome
+ */
+
+/*
+ * Mac
+ * g++ -std=c++14 <cpp_filename>
+ * ./a.out
+ */
+
+/*
+ * Windows
+ * g++ <cpp_filename>
+ * ./a.exe
+ */
+
+// #include <bits/stdc++.h>
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+typedef long long int ll;
+
+// Macro //
+#define X first
+#define Y second
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+// Global //
+
+
+// Functions //
+
+
+// Main //
+int main(int argc, char const *argv[])
+{
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    #else
+    // online submission
+    #endif
+    
+    /* code */
+    cout << "Everybody is Awesome!" << endl;
+    
+    return 0;
+}
