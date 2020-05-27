b0c9387d3bcb3a538795d83e16fee00afd6f1676
Pawan Harendra Mishra <pawanharendramishra@Pawan.local>
2020-05-27 16:16:05 +0530

    add binary_exponentiation

---
+++ binary_exponentiation.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+/* T is expected to be any integer data type */
+
+template <typename T>
+T bin_exp(T x, T y) {
+    if (y<=0) return 1;
+    T z = bin_exp(x, y/2);
+    return y%2 ? z*z*x : z*z;
+}
+
+template <typename T>
+T mod_bin_exp(T x, T y, T m) {
+    if (m<=1) return 0;
+
+    if (y<=0) return 1;
+    T z = mod_bin_exp(x, y/2, m);
+    return y%2 ? (z*z % m)*x % m : z*z % m;
+}
+
+template <typename T>
+T bin_exp_nr(T x, T y) {
+    if (y<=0) return 1;
+    
+    T res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x = x*x;
+        y >>= 1;
+    }
+    return res;
+}
+
+template <typename T>
+T mod_bin_exp_nr(T x, T y, T m) {
+    if (y<=0) return 1;
+    
+    T res = 1;
+    while (y) {
+        if (y & 1) res = res*x % m;
+        x = x*x % m;
+        y >>= 1;
+    }
+    return res;
+}
+
+int main(int argc, char const *argv[]) {
+    cout << bin_exp(2, 10) << endl;
+    cout << bin_exp_nr(2, 10) << endl;
+
+    cout << mod_bin_exp(2, 10, 3) << endl;
+    cout << mod_bin_exp_nr(2, 10, 3) << endl;
+    return 0;
+}
