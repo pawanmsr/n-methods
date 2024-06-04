f42cafefd399f14a1c084c7d0cd0624abfaa3d1f
Pawan Mishra <pawanmsr@outlook.com>
2024-06-04 14:15:07 +0530

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,40 @@
+#! /usr/bin/bash
 
-# Remove previous build.
-# Ideally one should only remove cmake_cache.
-rm -r build
+set -e
+
+display_options() {
+    echo "Syntax: bash build.sh [options]"
+    echo "Options"
+    echo "  -h  Print this Help."
+    echo "  -c  Force build from scratch."
+    echo
+}
+
+clean_build() {
+    rm -r ./build/*
+}
+
+BUILD_TOOL="cmake" # This project uses cmake.
+# Other build tools: meson, bazel, premake, etc.
+
+if command -v $BUILD_TOOL &>/dev/null ; then
+    while getopts hc flag ; do
+        case $flag in
+            c) # remove build directory
+                clean_build;;
+            h) # show help and exit
+                display_options
+                exit;;
+            \?) # show help but continue
+                display_options;;
+        esac
+    done
+
+    echo "Building using $BUILD_TOOL!"
+else
+    echo "Build tool not found!"
+    exit 1;
+fi
 
 # Prepare for build.
 cmake -S . -B ./build/
