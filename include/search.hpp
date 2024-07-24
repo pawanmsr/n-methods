1808bd07738dd47220a78b3abdb3c007f5801b5f
Pawan Mishra <pawanmsr@outlook.com>
2024-07-24 10:06:26 +0530

    format

--- include/search.hpp
+++ include/search.hpp
@@ -7,9 +7,10 @@
 namespace nm
 {
     template<class T, typename U>
-    U bound_search(T element, const U lo, const U hi, const std::vector<T>& space, std::function<bool(T&, T&)> compare = [](T& a, T& b) {
-        return a < b;
-    });
+    U bound_search(T element, const U lo, const U hi, const std::vector<T>& space,
+        std::function<bool(T&, T&)> compare = [](T& a, T& b) {
+            return a < b;
+        });
 } // namespace nm
 
 
--- include/sort.hpp
+++ include/sort.hpp
@@ -7,14 +7,16 @@
 namespace nm
 {
     template<class T, typename U>
-    void quick_sort(U lo, U hi, std::vector<T>& v, std::function<bool(T&, T&)> compare = [](T& a, T& b) {
-        return a < b;
-    });
+    void quick_sort(U lo, U hi, std::vector<T>& v,
+        std::function<bool(T&, T&)> compare = [](T& a, T& b) {
+            return a < b;
+        });
 
     template<class T, typename U, typename V>
-    V merge_sort(U lo, U hi, std::vector<T>& v, std::function<bool(T&, T&)> compare = [](T& a, T& b) {
-        return a < b;
-    });
+    V merge_sort(U lo, U hi, std::vector<T>& v,
+        std::function<bool(T&, T&)> compare = [](T& a, T& b) {
+            return a < b;
+        });
 } // namespace nm
 
 #endif // SORT
