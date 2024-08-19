abd57b171938dc4246f6d17185cd2ff8230526c3
Pawan H M <pawanmsr@outlook.com>
2024-08-19 10:57:42 +0530

    Vincent van Gogh
    Votre passion pour votre art
        reste dans mon memoire.
    It takes effort to see what you saw.

--- play/template.cpp
+++ play/template.cpp
@@ -110,7 +110,7 @@ int main(int argc, char const *argv[])
     #endif
     
     /* code */
-    cout << "On a windy moonlit night,\n"
+    cout << "On a starry moonlit night,\n"
     << "A lonely wolf begins to howl.\n"
     << "Wondering if his solutions are right,\n"
     << "'Do submit; maybe it's not all foul.'" << endl;
