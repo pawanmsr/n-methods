520863b1aa81e65524c14c7558d5e1ec5fde963c
Pawan Mishra <pawanmsr@outlook.com>
2024-09-11 21:08:06 +0530

    update segment tree

--- include/segment_tree.hpp
+++ include/segment_tree.hpp
@@ -14,10 +14,12 @@ namespace nm
         U integrator;
     protected:
         void construct(std::vector<T> &data, size_t lo, size_t hi, size_t i);
-        T traverse(size_t lo, size_t hi, size_t i, size_t tlo, size_t thi);
+        T query_tree(size_t lo, size_t hi, size_t tlo, size_t thi, size_t i);
+        bool update_tree(T value, size_t position, size_t lo, size_t hi, size_t i);
     public:
         SegmentTree(std::vector<T> &data, U *integrator_struct);
         T query(size_t left, size_t right);
+        bool update(T value, size_t position);
         ~SegmentTree();
     };
 } // namespace nm
--- src/segment_tree.cpp
+++ src/segment_tree.cpp
@@ -15,20 +15,42 @@ namespace nm
         }
     
     template<class T, class U>
-    T SegmentTree<T, U>::traverse(size_t lo, size_t hi, size_t tlo, size_t thi, size_t i) {
+    T SegmentTree<T, U>::query_tree(size_t lo, size_t hi, size_t tlo, size_t thi, size_t i) {
         if (lo > hi) return this->integrator->identity;
         if (lo == tlo and hi == thi)
             return this->tree[i];
         
         size_t mid = tlo + (thi - tlo) / 2;
-        return this->integrator->integrate(this->traverse(lo, min(mid, hi), tlo, mid, 2 * i),
-            this->traverse(max(lo, mid + 1), hi, mid + 1, thi, 2 * i + 1));
+        return this->integrator->integrate(this->query_tree(lo, min(mid, hi), tlo, mid, 2 * i),
+            this->query_tree(max(lo, mid + 1), hi, mid + 1, thi, 2 * i + 1));
+    }
+
+    template<class T, class U>
+    bool SegmentTree<T, U>::update_tree(T value, size_t position, size_t lo, size_t hi, size_t i) {
+        if (lo == hi) {
+            this->tree[i] = value;
+            return true;
+        }
+        
+        size_t mid = lo + (hi - lo) / 2;
+        if (position <= mid) return this->update_tree(value, position, lo, mid, 2 * i);
+        return this->update_tree(value, position, size_t mid + 1, size_t hi, 2 * i + 1);
+        
+        this->tree[i] = this->integrator->integrate(this->tree[2 * i], this->tree[2 * i + 1]);
+        
+        return true;
     }
     
     template<class T, class U>
     T SegmentTree<T, U>::query(size_t left, size_t right) {
         // validate left and right
-        return this->traverse(left, right, 0, this->n - 1, 0);
+        return this->query_tree(left, right, 0, this->n - 1, 0);
+    }
+
+    template<class T, class U>
+    bool SegmentTree<T, U>::update(T value, size_t position) {
+        // validate position
+        return update_tree(value, position, 0, this->n - 1, 0);
     }
     
     template<class T, class U>
