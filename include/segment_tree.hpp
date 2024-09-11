84131715ea6ae4051f07d687691a4dc295d44fb6
Pawan Mishra <pawanmsr@outlook.com>
2024-09-11 23:31:50 +0530

    add sample integrator

--- include/segment_tree.hpp
+++ include/segment_tree.hpp
@@ -5,8 +5,23 @@
 
 namespace nm
 {
+    // T is type of data to query on
+    template<class T>
+    struct U {
+        T identity;
+        T assign(T data) {
+            return data;
+        }
+
+        T integrate(T a, T b) {
+            // replace with operation on a and b
+            return identity;
+        }
+
+        U(T i) : identity(i) {};
+    };
+    
     /*
-     * T is data type of container.
      * U is a pointer to object or struct.
      * U must contain an identity element.
      * U must contain an 'assign' function.
@@ -25,7 +40,7 @@ namespace nm
         T query_tree(size_t lo, size_t hi, size_t tlo, size_t thi, size_t i);
         T update_tree(T value, size_t position, size_t lo, size_t hi, size_t i);
     public:
-        SegmentTree(std::vector<T> &data, U *integrator_struct);
+        SegmentTree(std::vector<T> &data, U *integrator_struct = new U());
         T query(size_t left, size_t right);
         T update(T value, size_t position);
         ~SegmentTree();
