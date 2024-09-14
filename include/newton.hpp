981f6610d7998b2d82ff1beeb3d739715531b11e
Pawan Mishra <pawanmsr@outlook.com>
2024-09-15 01:27:47 +0530

    declare nr solver

--- include/newton.hpp
+++ include/newton.hpp
@@ -1,7 +1,17 @@
 #if !defined(NEWTON)
 #define NEWTON
 
-// add methods here
+#include <functional>
+
 // overload for functions as arguments
+namespace nm
+{
+    // f is twice differentiable function
+    // f_prime is the derivative of f
+    // delta is small
+    // functional programming paradigm
+    template<typename T>
+    T newton(std::function<T(T&)> &f, std::function<T(T&)> &f_prime, const T delta);
+} // namespace nm
 
 #endif // NEWTON
