dee65adbded07eaae031cb44cb46345438630c70
Pawan Mishra <pawanmsr@outlook.com>
2024-09-15 14:01:27 +0530

    initial implementation

--- include/newton.hpp
+++ include/newton.hpp
@@ -8,10 +8,12 @@ namespace nm
 {
     // f is twice differentiable function
     // f_prime is the derivative of f
-    // delta is small
     // functional programming paradigm
+    // T is floating point type
+    // delta is small
     template<typename T>
-    T newton(std::function<T(T&)> &f, std::function<T(T&)> &f_prime, const T delta);
+    T newton(std::function<T(T&)> &f, std::function<T(T&)> &f_prime,
+        const T delta = T(0.01), T x = T(0.0));
 } // namespace nm
 
 #endif // NEWTON
--- src/newton.cpp
+++ src/newton.cpp
@@ -1,3 +1,18 @@
 #include <newton.hpp>
 
-// TODO
+namespace nm
+{
+    // refer
+    template<typename T>
+    T newton(std::function<T(T&)> &f, std::function<T(T&)> &f_prime, const T delta, T x) {
+        // add proof of convergence
+        
+        do {
+            T x_n = x - f(x) / f_prime(x);
+            T epsilon = x - x_n;
+            x = x_n;
+        } while (epsilon > delta);
+        
+        return x;
+    }
+} // namespace nm
