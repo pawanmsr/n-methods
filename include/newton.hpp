0771817bc0457374ebe2c0a4fbf836b5a638aace
Pawan Mishra <pawanmsr@outlook.com>
2024-10-01 22:46:09 +0530

    fix and instantiate

--- include/newton.hpp
+++ include/newton.hpp
@@ -6,7 +6,7 @@
 // overload for functions as arguments
 namespace nm
 {
-    std::size_t MAX_ITERATIONS = 1e5;
+    const std::size_t MAX_ITERATIONS = 1e5;
 
     // f is twice differentiable function
     // f_prime is the derivative of f
@@ -15,7 +15,7 @@ namespace nm
     // delta is small
     template<typename T>
     T newton(std::function<T(T)> &f, std::function<T(T)> &f_prime,
-        const T delta = T(0.01), T x = T(0.0), std::size_t iterations = MAX_ITERATIONS);
+        T delta = T(0.01), T x = T(0.0), std::size_t iterations = MAX_ITERATIONS);
 } // namespace nm
 
 #endif // NEWTON
--- src/newton.cpp
+++ src/newton.cpp
@@ -9,20 +9,28 @@ namespace nm
     // possible oscillations, multiplicity
     template<typename T>
     T newton(std::function<T(T)> &f, std::function<T(T)> &f_prime,
-        const T delta, T x, std::size_t iterations) {
+        T delta, T x, std::size_t iterations) {
             // add proof of convergence
             assert(iterations > 0);
             delta = std::abs(delta);
-            
-            do {
+            T epsilon = delta + 1.0;
+
+            while (epsilon > delta and iterations--) {
+                // TODO: add resolution for inflection
+                // point and extremum
                 if (f(x) == 0 and f_prime(x) == 0)
-                    x = f(iterations);
+                    break;
 
                 T x_n = x - f(x) / f_prime(x);
                 T epsilon = std::abs(x - x_n);
                 x = x_n;
-            } while (epsilon > delta and --iterations);
-            
+            }
+
             return x;
         }
 } // namespace nm
+
+template double nm::newton<double>(std::function<double(double)>&,
+    std::function<double(double)>&, double, double, unsigned long);
+template long double nm::newton<long double>(std::function<long double(long double)>&,
+    std::function<long double(long double)>&, long double, long double, unsigned long);
