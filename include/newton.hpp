4af92c8039a7105d258251480b11c769ce545bf3
Pawan Mishra <pawanmsr@outlook.com>
2024-10-01 21:45:52 +0530

    add iterations
    newton raphson
    inflection point resolution

--- include/newton.hpp
+++ include/newton.hpp
@@ -6,14 +6,16 @@
 // overload for functions as arguments
 namespace nm
 {
+    std::size_t MAX_ITERATIONS = 1e5;
+
     // f is twice differentiable function
     // f_prime is the derivative of f
     // functional programming paradigm
     // T is floating point type
     // delta is small
     template<typename T>
-    T newton(std::function<T(T&)> &f, std::function<T(T&)> &f_prime,
-        const T delta = T(0.01), T x = T(0.0));
+    T newton(std::function<T(T)> &f, std::function<T(T)> &f_prime,
+        const T delta = T(0.01), T x = T(0.0), std::size_t iterations = MAX_ITERATIONS);
 } // namespace nm
 
 #endif // NEWTON
--- src/newton.cpp
+++ src/newton.cpp
@@ -1,22 +1,28 @@
 #include <newton.hpp>
 
+#include <cassert>
+
 namespace nm
 {
     // one dimensional
     // refer quadratic convergence, concave and convex functions
     // possible oscillations, multiplicity
     template<typename T>
-    T newton(std::function<T(T&)> &f, std::function<T(T&)> &f_prime, const T delta, T x) {
-        // add proof of convergence
-        
-        delta = std::abs(delta);
-        
-        do {
-            T x_n = x - f(x) / f_prime(x);
-            T epsilon = std::abs(x - x_n);
-            x = x_n;
-        } while (epsilon > delta);
-        
-        return x;
-    }
+    T newton(std::function<T(T)> &f, std::function<T(T)> &f_prime,
+        const T delta, T x, std::size_t iterations) {
+            // add proof of convergence
+            assert(iterations > 0);
+            delta = std::abs(delta);
+            
+            do {
+                if (f(x) == 0 and f_prime(x) == 0)
+                    x = f(iterations);
+
+                T x_n = x - f(x) / f_prime(x);
+                T epsilon = std::abs(x - x_n);
+                x = x_n;
+            } while (epsilon > delta and --iterations);
+            
+            return x;
+        }
 } // namespace nm
