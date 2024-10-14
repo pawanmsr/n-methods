7d568059d055a66e73477750cf3f3f5f2a7b7ad0
Pawan Mishra <pawanmsr@outlook.com>
2024-10-15 00:41:49 +0530

    copy extended gcd to gcd

--- include/gcd.hpp
+++ include/gcd.hpp
@@ -8,6 +8,12 @@ namespace nm
 
     template <typename T>
     T gcd_iterative(T a, T b);
+
+    template <typename T>
+    T extended_gcd(T a, T b, T& x, T& y);
+
+    template <typename T>
+    T extended_gcd_iterative(T a, T b, T& x, T& y);
 } // namespace nm
 
 #endif // GCD
