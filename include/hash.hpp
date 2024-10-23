6d7bde76146e45c123e9393d2111f4baa1e7477e
Pawan Mishra <pawanmsr@outlook.com>
2024-10-23 23:58:15 +0530

    add structure

--- include/hash.hpp
+++ include/hash.hpp
@@ -3,12 +3,14 @@
 
 namespace nm
 {
+    template<typename T>
     class Linear
     {
     private:
         std::size_t m;
     public:
         Linear(std::size_t mod);
+        T hash(T x);
         ~Linear();
     };
     
