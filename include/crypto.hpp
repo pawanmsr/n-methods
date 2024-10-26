c165e42a2460bdbcbfc50d453f363d27e5f54fb1
Pawan Mishra <pawanmsr@outlook.com>
2024-10-26 18:55:31 +0530

    add private

--- include/crypto.hpp
+++ include/crypto.hpp
@@ -1,10 +1,20 @@
 #if !defined(CRYPTO)
 #define CRYPTO
 
+#include <vector>
+#include <string>
+#include <cstddef>
+#include <functional>
 namespace nm
 {
     class DES {
     private:
+        // key optionals
+        std::string key;
+        std::vector<unsigned char> uc_key;
+        std::vector<std::byte> byte_key;
+
+        std::function<std::byte(std::byte, std::byte)> feistel;
     public:
         DES();
         void encrypt();
