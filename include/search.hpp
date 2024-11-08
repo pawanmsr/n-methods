bd37895dfbd4223c1fe90e69a27a32f26cc194b5
Pawan Mishra <pawanmsr@outlook.com>
2024-11-08 21:33:03 +0530

    clear memory

--- include/search.hpp
+++ include/search.hpp
@@ -7,8 +7,7 @@
 #include <string>
 #include <functional>
 
-namespace nm
-{
+namespace nm {
     template<class T, typename U>
     U bound_search(T element, const U lo, const U hi, const std::vector<T>& space,
         std::function<bool(T&, T&)> compare = default_compare<T>);
@@ -18,8 +17,10 @@ namespace nm
         std::vector<int32_t> positions;
         std::vector<int32_t> partial;
         std::vector<int32_t> prefix;
+        
         std::string w;
         std::string s; // input
+        
         std::size_t n; // input length
         std::int32_t i; // processed
     
@@ -29,8 +30,12 @@ namespace nm
         
     public:
         KMP(std::string word);
-        void append(std::string s);
+        
+        void clear();
+        void flush();
+        
         void stream(char c);
+        void stream(std::string s);
         std::vector<int32_t> search();
         
         ~KMP();
