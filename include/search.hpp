eed57e357a732fe73f0f11d3fe6f8645c22acf9c
Pawan Mishra <pawanmsr@outlook.com>
2024-11-10 13:29:00 +0530

    critical memory

--- include/search.hpp
+++ include/search.hpp
@@ -8,17 +8,21 @@
 #include <functional>
 
 namespace nm {
+    // 64 MegaBytes
+    const size_t SIZE_LIMIT_MEM = 64 * 1024 * 1024;
+
     template<class T, typename U>
     U bound_search(T element, const U lo, const U hi, const std::vector<T>& space,
         std::function<bool(T&, T&)> compare = default_compare<T>);
 
     class KMP {
     private:
-        std::vector<int32_t> positions;
-        std::vector<int32_t> partial;
-        std::vector<int32_t> prefix;
+        std::vector<std::int32_t> positions;
+        std::vector<std::int32_t> partial;
+        std::vector<std::int32_t> prefix;
 
         bool case_sensitive;
+        std::size_t critical_memory;
         
         std::string w;
         std::string s; // input
@@ -27,12 +31,15 @@ namespace nm {
         std::int32_t i; // processed
     
     protected:
+        std::size_t memory(bool all = false);
         void prefix_function();
         void failure_function();
         bool compare(char x, char y);
         
     public:
-        KMP(std::string word, bool case_sensitive = true);
+        KMP(std::string word,
+            bool case_sensitive = true,
+                std::size_t memory = SIZE_LIMIT_MEM);
         
         void clear();
         void flush();
@@ -45,6 +52,5 @@ namespace nm {
     };
     
 } // namespace nm
-
 
 #endif // SEARCH
