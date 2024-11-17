675a2d5ab6c8d6178cc86a34c5d4a51c37b00f6d
Pawan Mishra <pawanmsr@outlook.com>
2024-11-18 00:28:05 +0530

    template file

---
+++ play/template.rs
@@ -0,0 +1,28 @@
+// Imports //
+use std::io;
+
+// Macro //
+
+
+// External //
+
+
+// Global //
+
+
+// Functions //
+
+
+fn main() {
+    let mut s = String::new();
+    
+    let mut t: usize = 1;
+    // comment lines below for single test case
+    io::stdin().read_line(&mut s).unwrap();
+    t = s.trim().parse::<usize>().unwrap();
+    s.clear();
+    
+    for _ in 0..t {
+        // solution //
+    }
+}
