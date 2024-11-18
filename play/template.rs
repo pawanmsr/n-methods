9e7e64b9fc0e2bd21068bf9dc923c18f09260c4f
Pawan Mishra <pawanmsr@outlook.com>
2024-11-18 23:35:17 +0530

    update template

--- play/template.rs
+++ play/template.rs
@@ -22,9 +22,8 @@ fn main() -> io::Result<()> {
     let stdin: String = io::read_to_string(io::stdin()).unwrap();
     let mut s: std::str::SplitWhitespace<'_> = stdin.split_whitespace();
     
-    let mut t: usize = 1;
-    // comment the line below for single test case
-    t = s.next().unwrap().parse::<usize>().unwrap();
+    // let t: usize = 1; // for single test
+    let t: usize = s.next().unwrap().parse::<usize>().unwrap();
     
     for _ in 0..t {
         // solution //
