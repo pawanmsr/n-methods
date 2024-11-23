4eebe9ea55368441223a7da5a49ae6b96b795de7
Pawan H M <25368219+pawanmsr@users.noreply.github.com>
2024-11-23 23:04:29 +0530

    Template (#53)

--- play/template.rs
+++ play/template.rs
@@ -4,11 +4,17 @@
  */
 
 // #![warn(unused_macros)]
+// #![warn(unused_imports)]
+// #![warn(unused_must_use)]
 // #![allow(unused_variables)]
 
 // Standard Library //
 use std::io;
 use std::fs;
+
+use std::io::Write;
+
+use std::cmp::{min, max, Ordering};
 
 // use std::collections::{BinaryHeap, VecDeque};
 // use std::collections::{BTreeMap, BTreeSet];
@@ -17,15 +23,15 @@ use std::fs;
 
 // Macro //
 macro_rules! input {
-    ($t: ty, $s: expr) => {
+    ($s: expr, $t: ty) => {
         $s.next().unwrap().parse::<$t>().unwrap()
     };
 
-    (String, $s: expr) => {
+    ($s: expr, String) => {
         $s.next().unwrap().to_string()
     };
 
-    ($t: ty, $n : expr, $s: expr) => {
+    ($s: expr, $t: ty, $n : expr) => {
         (0..$n)
             .map(|_| $s.next().unwrap().parse::<$t>().unwrap())
             .collect::<Vec<$t>>()
@@ -46,15 +52,22 @@ fn main() -> io::Result<()> {
     let stdin: String = io::read_to_string(io::stdin().lock()).unwrap();
     #[cfg(any(LOCAL, feature = "LOCAL"))]
     let stdin: String = fs::read_to_string("input.txt").unwrap();
+
+    #[cfg(not(LOCAL))]
+    let stdout  = io::stdout().lock();
+    #[cfg(any(LOCAL, feature = "LOCAL"))]
+    let stdout = fs::File::create("output.txt").unwrap();
     
-    let mut s: std::str::SplitWhitespace<'_> = stdin.split_whitespace();
+    let mut cin = stdin.split_whitespace();
+    let mut cout = stdout;
     
+    // Solution //
     // let t: usize = 1; // for single test
-    let t: usize = input!(usize, s);
+    let t: usize = input!(cin, usize);
     for _ in 0..t {
-        // Solution //
-        println!("Everyone is awesome!");
+        // Test Case //
     }
 
+    dbg!("Everyone is awesome!");
     return Ok(());
 }
