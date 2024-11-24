db1ae5208ae0a25f2bd4cfd8d6af7c5d71738b4e
Pawan Mishra <pawanmsr@outlook.com>
2024-11-25 00:31:43 +0530

    add graph

---
+++ src/graph.rs
@@ -0,0 +1,15 @@
+use std::collections::HashSet;
+
+struct Graph {
+    n: usize,
+    directed: bool,
+    adjacency_list: Vec<HashSet<usize> >,
+}
+
+impl Graph {
+    fn add_edge(u: usize, v: usize) {
+    }
+
+    fn mst() {
+    }
+}
