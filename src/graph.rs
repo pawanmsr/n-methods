67266a5cc2f28bf9685dbcd6c3b27de5659a79cd
Pawan Mishra <pawanmsr@outlook.com>
2024-11-25 17:36:11 +0530

    add minimum spanning tree
    kruskal
    union find

--- src/graph.rs
+++ src/graph.rs
@@ -1,7 +1,60 @@
 use std::collections::HashMap;
-use std::hash::Hasher
+use std::mem::swap;
 
 pub struct UnionFind {
+    n: usize,
+    parent: Vec<i64>,
+}
+
+impl UnionFind {
+    pub fn new(n: usize) -> Self {
+        Self {
+            n,
+            parent: vec![-1; n],
+        }
+    }
+
+    pub fn find(&self, x: usize) -> usize {
+        assert!(x < self.n);
+        
+        let mut x: i64 = x as i64;
+        while self.parent[x as usize] > 0 {
+            x = self.parent[x as usize];
+        }
+
+        return x as usize;
+    }
+
+    pub fn size(&self, x: usize) -> usize {
+        let x: usize = self.find(x);
+        return (-self.parent[x]) as usize;
+    }
+
+    pub fn unite(&mut self, x: usize, y: usize) -> usize {
+        let mut x: usize = self.find(x);
+        let mut y: usize = self.find(y);
+
+        if x == y {
+            return x;
+        }
+
+        let size_of_x: usize = self.size(x);
+        let size_of_y: usize = self.size(y);
+
+        let component_size: usize = size_of_x + size_of_y;
+        if size_of_x > size_of_y {
+            swap(&mut x, &mut y);
+        }
+
+        self.parent[x] = y as i64;
+        self.parent[y] = -(component_size as i64);
+
+        return y;
+    }
+
+    pub fn united(&self, x: usize, y: usize) -> bool {
+        return self.find(x) == self.find(y);
+    }
 }
 
 pub struct Graph {
@@ -14,10 +67,10 @@ pub struct Graph {
 impl Graph {
     pub fn new(n: usize, directed: bool, weighted: bool) -> Self {
         Self {
-            n: n + 1,
+            n,
             directed,
             weighted,
-            adjacency_list: vec![HashMap::<usize, i64>::new(); n + 1],
+            adjacency_list: vec![HashMap::<usize, i64>::new(); n],
         }
     }
 
@@ -36,6 +89,36 @@ impl Graph {
         self.adjacency_list[v].insert(u, w);
     }
 
-    fn mst() {
+    pub fn get_edges(&self) -> Vec<(usize, usize, i64)> {
+        let mut edges: Vec<(usize, usize, i64)> = Vec::new();
+        for u in 0..self.n {
+            for (v, w) in &self.adjacency_list[u] {
+                if ! self.directed && u > *v {
+                    continue;
+                }
+
+                edges.push((u, *v, *w));
+            }
+        }
+
+        return edges;
+    }
+
+    pub fn mst(&self) -> Vec<(usize, usize, i64)> {
+        let mut mst: Vec<(usize, usize, i64)> = Vec::new();
+        let mut uf: UnionFind = UnionFind::new(self.n);
+        let mut edges: Vec<(usize, usize, i64)> = self.get_edges();
+        edges.sort_by_key(|(_, _, w)| *w);
+
+        for (u, v, w) in edges {
+            if uf.united(u, v) {
+                continue;
+            }
+
+            mst.push((u, v, w));
+            uf.unite(u, v);
+        }
+        
+        return mst;
     }
 }
