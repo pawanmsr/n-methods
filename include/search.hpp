b42f22704edfcfe498c9c08d5db7ae8f093a5a48
Pawan Mishra <pawanmsr@outlook.com>
2024-12-05 20:36:48 +0530

    encapsulate classes

--- include/search.hpp
+++ include/search.hpp
@@ -15,20 +15,35 @@ namespace nm {
     U bound_search(T element, const U lo, const U hi, const std::vector<T>& space,
         std::function<bool(T&, T&)> compare = default_compare<T>);
 
-    class KMP {
+    class SS {
+    protected:
+        bool case_sensitive;
+        std::size_t critical_memory;
+
+        std::string w;
+        std::string s; // input
+
+        std::size_t n; // input length
+        std::int32_t i; // processed
+    public:
+        SS(std::string word,
+            bool case_sensitive = true,
+                std::size_t memory = SIZE_LIMIT_MEM);
+        
+        void clear();
+        void flush();
+        
+        void stream(char c);
+        void stream(std::string s);
+
+        ~SS();
+    };
+    
+    class KMP : public SS {
     private:
         std::vector<std::int32_t> positions;
         std::vector<std::int32_t> partial;
         std::vector<std::int32_t> prefix;
-
-        bool case_sensitive;
-        std::size_t critical_memory;
-        
-        std::string w;
-        std::string s; // input
-        
-        std::size_t n; // input length
-        std::int32_t i; // processed
     
     protected:
         std::size_t memory(bool all = false);
@@ -42,15 +57,10 @@ namespace nm {
                 std::size_t memory = SIZE_LIMIT_MEM);
         
         void clear();
-        void flush();
-        
-        void stream(char c);
-        void stream(std::string s);
         std::vector<int32_t> search();
         
         ~KMP();
     };
-    
 } // namespace nm
 
 #endif // SEARCH
