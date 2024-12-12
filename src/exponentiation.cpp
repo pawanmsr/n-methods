ea2a3102529dbec4fb63e755e603e7144348c8c6
Pawan Mishra <pawanmsr@outlook.com>
2024-12-12 19:42:04 +0530

    move mod functions

--- src/exponentiation.cpp
+++ src/exponentiation.cpp
@@ -1,7 +1,6 @@
 #include <exponentiation.hpp>
 
-namespace nm
-{
+namespace nm {
     /* T is expected to be any integer data type */
     template <typename T>
     T bin_exp(T x, T y) {
@@ -9,15 +8,6 @@ namespace nm
         
         T z = bin_exp<T>(x, y/2);
         return y%2 ? z*z*x : z*z;
-    }
-
-    template <typename T>
-    T mod_bin_exp(T x, T y, const T m) {
-        if (m<=1) return 0;
-        if (y<=0) return 1;
-        
-        T z = mod_bin_exp<T>(x, y/2, m);
-        return y%2 ? (z*z % m)*x % m : z*z % m;
     }
 
     template <typename T>
@@ -32,33 +22,12 @@ namespace nm
         }
         return res;
     }
-
-    template <typename T>
-    T mod_bin_exp_iterative(T x, T y, const T m) {
-        if (y<=0) return 1;
-        
-        T res = 1;
-        while (y) {
-            if (y & 1) res = res*x % m;
-            x = x*x % m;
-            y >>= 1;
-        }
-        return res;
-    }
 } // namespace nm
 
 template int nm::bin_exp<int>(int, int);
 template long nm::bin_exp<long>(long, long);
 template long long nm::bin_exp<long long>(long long, long long);
 
-template int nm::mod_bin_exp<int>(int, int, int);
-template long nm::mod_bin_exp<long>(long, long, long);
-template long long nm::mod_bin_exp<long long>(long long, long long, long long);
-
 template int nm::bin_exp_iterative<int>(int, int);
 template long nm::bin_exp_iterative<long>(long, long);
 template long long nm::bin_exp_iterative<long long>(long long, long long);
-
-template int nm::mod_bin_exp_iterative<int>(int, int, int);
-template long nm::mod_bin_exp_iterative<long>(long, long, long);
-template long long nm::mod_bin_exp_iterative<long long>(long long, long long, long long);
