4c27180574ef3babf2844d999fbe4490138194d9
Pawan Mishra <pawanmsr@outlook.com>
2024-12-18 11:23:27 +0530

    fix hash function

--- src/hash.cpp
+++ src/hash.cpp
@@ -4,9 +4,11 @@ namespace nm {
     template<std::int64_t M, std::int64_t P>
     Hash<M, P>::Hash(std::string &s) : Arithmetic<std::int64_t>::Arithmetic(M) {
         this->n = s.length();
-        this->hash.resize(this->n + 1);
-        this->power.resize(this->n + 1, 1);
+        this->hash.reserve(this->n + 1);
+        this->power.reserve(this->n + 1);
 
+        this->hash = {0};
+        this->power = {1};
         for (char s_i : s) {
             this->hash.push_back(this->add(this->multiply(this->hash.back(), P), s_i));
             this->power.push_back(this->multiply(this->power.back(), P));
@@ -15,7 +17,7 @@ namespace nm {
 
     template<std::int64_t M, std::int64_t P>
     std::int64_t Hash<M, P>::Interval(std::size_t left, std::size_t right) {
-        assert(left >= 0 and right < this->n and right >= left);
+        assert(right <= this->n and right >= left);
         return this->subtract(this->hash[right],
             this->multiply(this->hash[left],
                 this->power[right - left]));
