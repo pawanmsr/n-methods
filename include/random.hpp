e1258c2099e8cc25b72814305639331769e67952
Pawan Mishra <pawanmsr@outlook.com>
2025-01-05 22:52:28 +0530

    add rng

--- include/random.hpp
+++ include/random.hpp
@@ -1,6 +1,29 @@
 #if !defined(RANDOM)
 #define RANDOM
 
-// Mersenne primes
+#include <cstdint>
+
+#include <modulo.hpp>
+
+namespace nm {
+    /*
+     * X_{L} and X_{R} are same terminology as in
+     *   Surreal Numbers, Knuth.
+     *
+     * TODO: generate floating points numbers.
+     *  by concatenating number from 0-9.
+     */
+    class Random {
+        private:
+            int32_m n;
+        public:
+            Random();
+            Random(int32_m s);
+            std::size_t number(std::size_t xr);
+            std::size_t number(std::size_t xl, std::size_t xr);
+            ~Random();
+    }
+} // random numbers
+
 
 #endif // RANDOM
--- src/random.cpp
+++ src/random.cpp
@@ -1,5 +1,29 @@
 #include <random.hpp>
 
+#include <chrono>
+
 namespace nm {
-    
-} // namespace nm
+    Random::Random() {
+        this->n = std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+    }
+
+    Random::Random(int32_m s) {
+        this->n = s;
+    }
+
+    inline std::size_t Random::number(std::size_t xr) {
+        return this->number(0, xr);
+    }
+
+    std::size_t Random::number(std::size_t xl, std::size_t xr) {
+        std::size_t mod = xr - xl;
+        int32_m p = std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+        this->n *= p;
+
+        return int(this->n) % mod;
+    }
+
+    Random::~Random() {};
+} // random numbers
