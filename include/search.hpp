1e160cef5cad4bb5559d8cdf64c5e54e64fee60c
Pawan H M <25368219+pawanmsr@users.noreply.github.com>
2025-01-08 23:27:06 +0530

    BMA (#57)

--- include/search.hpp
+++ include/search.hpp
@@ -8,36 +8,31 @@
 #include <functional>
 
 namespace nm {
-    // 64 MegaBytes
-    const size_t SIZE_LIMIT_MEM = 64 * 1024 * 1024;
+    const std::size_t ASCII = 256; // 0-indexed - printable above 31
+    const std::size_t UTF_EIGHT = 1000;
+    const std::size_t SIZE_LIMIT_MEM = 64 * 1024 * 1024; // 64 MegaBytes
 
     template<class T, typename U>
     U bound_search(T element, const U lo, const U hi, const std::vector<T>& space,
         std::function<bool(T&, T&)> compare = default_compare<T>);
 
-    class KMP {
-    private:
-        std::vector<std::int32_t> positions;
-        std::vector<std::int32_t> partial;
-        std::vector<std::int32_t> prefix;
-
+    /*
+     * String Search
+     * Base class for string search algorithms.
+     */
+    class SS {
+    protected:
         bool case_sensitive;
         std::size_t critical_memory;
-        
+        bool compare(char x, char y);
+
         std::string w;
         std::string s; // input
-        
+
         std::size_t n; // input length
         std::int32_t i; // processed
-    
-    protected:
-        std::size_t memory(bool all = false);
-        void prefix_function();
-        void failure_function();
-        bool compare(char x, char y);
-        
     public:
-        KMP(std::string word,
+        SS(std::string word,
             bool case_sensitive = true,
                 std::size_t memory = SIZE_LIMIT_MEM);
         
@@ -46,11 +41,61 @@ namespace nm {
         
         void stream(char c);
         void stream(std::string s);
-        std::vector<int32_t> search();
-        
-        ~KMP();
+
+        ~SS();
     };
     
+    /*
+     * Knuth, Morris, Pratt
+     * String search algorithm.
+     */
+    class KMP : public SS {
+        private:
+            std::vector<std::int32_t> positions;
+            std::vector<std::int32_t> partial;
+            std::vector<std::int32_t> prefix;
+    
+        protected:
+            std::size_t memory(bool all = false);
+            void prefix_function();
+            void failure_function();
+        
+        public:
+            KMP(std::string word,
+                bool case_sensitive = true,
+                    std::size_t memory = SIZE_LIMIT_MEM);
+            
+            void clear();
+            std::vector<std::int32_t> search();
+        
+            ~KMP();
+    };
+
+    /*
+     * Boyar (Robert S.) and Moore (J. Strother) Algorithm
+     * https://www.cs.utexas.edu/~moore/best-ideas/string-searching/index.html
+     */
+    class BMA : public SS {
+        private:
+            std::vector<std::uint32_t> delta_one;
+            std::vector<std::uint32_t> delta_two;
+            std::vector<std::uint32_t> positions;
+            std::vector<std::uint32_t> delta_zero;
+
+        protected:
+            void delta_function();
+            std::size_t memory(bool all = false);
+
+        public:
+            BMA(std::string word,
+                bool case_sensitive = true,
+                    std::size_t memory = SIZE_LIMIT_MEM);
+            
+            void clear();
+            std::vector<std::uint32_t> search();
+            
+            ~BMA();
+    };
 } // namespace nm
 
 #endif // SEARCH
--- src/search.cpp
+++ src/search.cpp
@@ -1,7 +1,8 @@
 #include <search.hpp>
 
-namespace nm
-{
+#include <functional>
+
+namespace nm {
     // lo: lower index, hi: higher index
     template<class T, typename U>
     U bound_search(T element, const U lo, const U hi,
@@ -28,10 +29,49 @@ template int nm::bound_search<int, int>(int, const int, const int,
 template int nm::bound_search<long long, int>(long long, const int, const int,
     const std::vector<long long>&, std::function<bool(long long&, long long&)>);
 
+namespace nm {
+    SS::SS(std::string word, bool case_sensitive, std::size_t memory) :
+    w(word), case_sensitive(case_sensitive), critical_memory(memory) {}
+
+    bool SS::compare(char x, char y) {
+        auto lower_case = [] (char z) {
+            if (z >= 'A' and z <= 'Z')
+                z = 'a' + (z - 'A');
+            
+            return z;
+        };
+
+        if (this->case_sensitive) return x == y;
+        return lower_case(x) == lower_case(y);
+    }
+
+    void SS::flush() {
+        this->s.clear();
+        this->n = 0;
+        this->i = 0;
+    }
+
+    void SS::clear() {
+        w.clear();
+        this->flush();
+    }
+
+    void SS::stream(std::string s) {
+        this->s += s;
+        this->n += s.length();
+    }
+    
+    void SS::stream(char c) {
+        this->s += c;
+        this->n++;
+    }
+
+    SS::~SS() {}
+}
 
 namespace nm {
     KMP::KMP(std::string word, bool case_sensitive, std::size_t memory) :
-        w(word), case_sensitive(case_sensitive), critical_memory(memory) {
+        SS(word, case_sensitive, memory) {
         this->flush();
         this->prefix = {0};
         this->partial = {-1};
@@ -51,27 +91,16 @@ namespace nm {
         return m;
     }
 
-    bool KMP::compare(char x, char y) {
-        auto lower_case = [] (char z) {
-            if (z >= 'A' and z <= 'Z')
-                z = 'a' + (z - 'A');
-            
-            return z;
-        };
-
-        if (this->case_sensitive) return x == y;
-        return lower_case(x) == lower_case(y);
-    }
-
     void KMP::prefix_function() {
-        while (this->i < this->n) {
-            std::int32_t j = this->partial[i - 1];
-            while (j > 0 and not compare(this->s[j], this->s[i]))
-                j = this->partial[j - 1];
+        const std::size_t len_w = this->w.length();
+        
+        for (std::size_t i = 1; i < len_w; i++) {
+            std::int32_t j = this->prefix[i - 1];
+            while (j > 0 and not this->compare(this->w[j], this->w[i]))
+                j = this->prefix[j - 1];
             
-            if (this->s[i] == this->s[j]) j++;
-            this->partial.push_back(j);
-            this->i++;
+            if (this->compare(this->w[j], this->w[i])) j++;
+            this->prefix.push_back(j);
         }
 
         if (memory() > this->critical_memory)
@@ -83,7 +112,7 @@ namespace nm {
         std::size_t len_w = this->w.size();
         
         while (this->i < this->n) {
-            if (compare(this->w[k], this->s[i])) {
+            if (this->compare(this->w[k], this->s[i])) {
                 this->i++;
 
                 k++;
@@ -128,31 +157,127 @@ namespace nm {
         this->partial.push_back(i);
     }
 
-    void KMP::flush() {
-        this->s.clear();
-        this->n = 0;
-        this->i = 0;
-    }
-
     void KMP::clear() {
         this->positions.clear();
         this->partial.clear();
         this->prefix.clear();
 
-        w.clear();
+        SS::clear();
+    }
+    
+    KMP::~KMP() {}
+} // namespace nm
+
+namespace nm {
+    BMA::BMA(std::string word, bool case_sensitive, std::size_t memory) : 
+        SS(word, case_sensitive, memory) {
         this->flush();
+        this->delta_function();
     }
 
-    void KMP::stream(std::string s) {
-        this->s += s;
-        this->n += s.length();
+    void BMA::delta_function() {
+        const std::size_t len_w = this->w.length();
+        
+        this->delta_one.resize(ASCII, len_w);
+        for (std::size_t j = len_w - 1; j >= 0; j--) {
+            if (this->delta_one[this->w[i]] == len_w)
+                this->delta_one[this->w[i]] -= j + 1;
+        }
+
+        std::uint32_t prefix_j = 0;
+        // prefix_j is the smallest j for which there is prefix for suffix starting from j + 1
+        std::function<std::int32_t(std::int32_t)> rpr = [&] (const std::size_t j) -> std::int32_t {
+            // rightmost plausible reoccurrence //
+            std::uint32_t len_suffix = len_w - (j + 1);
+            
+            bool is_prefix = true;
+            for (std::uint32_t k = 0; k < len_suffix; k++) {
+                if (this->w[k] == this->w[j + 1 + k]) continue;
+                
+                is_prefix = false;
+                break;
+            }
+            
+            if (is_prefix) prefix_j = j;
+
+            for (std::int32_t k = len_w - 1; k >= 0; k--) {
+                std::int32_t j_prime = len_w - 1;
+                
+                if (j + 1 > j_prime) break;
+
+                std::int32_t k_prime = k;
+                while (k_prime >= 0 and j_prime > j) {
+                    if (this->w[k_prime] != this->w[j_prime]) break;
+                    
+                    k_prime--;
+                    j_prime--;
+                }
+                
+                if (k_prime >= 0 and j_prime == j and 
+                    this->w[k_prime] != this->w[j_prime])
+                        return k + 1 - len_suffix; // verify
+            }
+
+            return j - prefix_j; // verify
+        };
+
+        this->delta_two.resize(len_w);
+        for (std::size_t j = len_w - 1; j >= 0; j--)
+            this->delta_two[j] = len_w - rpr(j);
+    }
+
+    std::size_t BMA::memory(bool all) {
+        std::size_t m = sizeof(this->s) + sizeof(char) * this->n;
+        if (not all) return m; // memory in bytes
+        
+        m += sizeof(this->positions) + 
+            sizeof(std::uint32_t) * this->positions.size();
+        m += sizeof(this->delta_one) +
+            sizeof(std::uint32_t) * this->delta_one.size();
+        m += sizeof(this->delta_two) +
+            sizeof(std::uint32_t) * this->delta_two.size();
+        m += sizeof(this->delta_zero) +
+            sizeof(std::uint32_t) * this->delta_zero.size();
+        
+        return m;
+    }
+
+    void BMA::clear() {
+        this->positions.clear();
+
+        SS::clear();
+    }
+
+    std::vector<std::uint32_t> BMA::search() {
+        const std::size_t len_w = this->w.size();
+
+        // adapted for understandability
+        std::int32_t j, i = this->i + len_w;
+        while (i < this->n) {
+            this->i = i;
+            j = len_w - 1;
+
+            
+            // analysis pending //
+            // slow implementation //
+            // sequence is matched in backwards order //
+            while (j >= 0 and this->compare(this->s[i], this->w[j])) {
+                i--;
+                j--;
+            }
+
+            if (j < 0) {
+                // found a match //
+                this->positions.push_back(i + 1);
+                i += len_w;
+                continue;
+            }
+
+            i += std::max(this->delta_one[this->s[i]], this->delta_two[j]);
+        }
+
+        return this->positions;
     }
     
-    void KMP::stream(char c) {
-        this->s += c;
-        this->n++;
-    }
-    
-    KMP::~KMP() {
-    }
-} // namespace nm
+    BMA::~BMA() {}
+} // sublinear string search
