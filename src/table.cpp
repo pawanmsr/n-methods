3b209bdc7cabd4ff85e25d140bb5877696512bfe
Pawan Mishra <pawanmsr@outlook.com>
2025-01-16 00:24:25 +0530

    add query

--- src/table.cpp
+++ src/table.cpp
@@ -23,6 +23,24 @@ namespace nm {
             }
         }
     }
+
+    template<typename T>
+    T Sparse<T>::query(std::size_t l, std::size_t r) {
+        // for non cumulative queries
+        int i = bit_width(r - l + 1UL) - 1;
+        return this->operation(this->table[i][l], this->table[i][r - (1 << i) + 1]);
+
+        // for cumulative queries
+        T result = this->operation(0, 0); // modify
+        for (std::int32_t i = this->k; i >= 0; i--) {
+            if ((1 << i) > r - l + 1) continue;
+            
+            result = this->operation(result, this->table[i][l])
+            l += 1 << i;
+        }
+        
+        return result;
+    }
     
     template<typename T>
     Sparse<T>::~Sparse() {}
