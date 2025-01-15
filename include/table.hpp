83f9301e09219206e66cd156262485626aa32157
Pawan Mishra <pawanmsr@outlook.com>
2025-01-16 00:20:22 +0530

    sparse table

--- include/table.hpp
+++ include/table.hpp
@@ -1,8 +1,22 @@
 #if !defined(TABLE)
 #define TABLE
 
+#include <vector>
+#include <functional>
+
 namespace nm {
-    
+    template<typename T>
+    class Sparse {
+        private:
+            std::size_t k;
+            std::size_t n;
+            std::vector<vector<T> > table
+            std::function<T(T, T)> operation;
+        public:
+            Sparse(const std::vector<T> &data, std::function<T(T, T)> &f);
+            T query(std::size_t l, std::size_t r);
+            ~Sparse();
+    };
 }
 
 #endif
