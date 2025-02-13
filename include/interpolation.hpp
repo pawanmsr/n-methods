9c898f782e555974884be7aa5d4bbd1138473763
Pawan Mishra <pawanmsr@outlook.com>
2025-02-13 19:52:09 +0530

    member function decl

--- include/interpolation.hpp
+++ include/interpolation.hpp
@@ -78,15 +78,27 @@ namespace nm {
         private:
             std::size_t n;
             std::vector<std::vector<long double> > A;
+        protected:
+            void normalize();
+            
+            // Add solvers
+            void gauss();
+            void cramer();
+            void fourier();
         public:
             SLE() {this->n = -1;};
             SLE(std::vector<std::vector<long double> > matrix);
             SLE(std::size_t nr, std::size_t nc);
             void add_row(std::vector<long double> row);
-            ~SLE();
+            std::size_t rank();
+            std::vector<long double> eigenvalues();
+            
+            // rank equals n
+            long double determinant();
+            SLE inverse();
+
+            ~SLE() {};
     };
-    
-    SLE::~SLE() {}
 }
 
 namespace nm {
