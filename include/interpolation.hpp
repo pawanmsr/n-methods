d48ca9023cbdee69423cf97e9c0ded731691e577
Pawan Mishra <pawanmsr@outlook.com>
2025-03-07 23:18:00 +0530

    fix rebase failure

--- include/interpolation.hpp
+++ include/interpolation.hpp
@@ -71,36 +71,6 @@ namespace nm {
     };
 } // system of linear equations
 
-// TODO: #include <stdfloat>
-
-namespace nm {
-    class SLE {
-        private:
-            std::size_t n;
-            std::vector<std::vector<long double> > A;
-        protected:
-            void normalize();
-            
-            // Add solvers
-            void gauss();
-            void cramer();
-            void fourier();
-        public:
-            SLE() {this->n = -1;};
-            SLE(std::vector<std::vector<long double> > matrix);
-            SLE(std::size_t nr, std::size_t nc);
-            void add_row(std::vector<long double> row);
-            std::size_t rank();
-            std::vector<long double> eigenvalues();
-            
-            // rank equals n
-            long double determinant();
-            SLE inverse();
-
-            ~SLE() {};
-    };
-} // system of linear equations
-
 namespace nm {
     template<typename T>
     std::function<T(T)> polynomial(const std::vector<T> &coefficients);
