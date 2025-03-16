7f7af38abf75fa7ecea903e4d9ded233eb09c8df
Pawan Mishra <pawanmsr@outlook.com>
2025-03-17 00:44:22 +0530

    add mod hash and cc

--- include/hash.hpp
+++ include/hash.hpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 
 #include <modulo.hpp>
+#include <trees.hpp>
 
 namespace nm {
     template <std::int64_t M, std::int64_t P>
@@ -21,5 +22,36 @@ namespace nm {
     };
 
 } // string
+
+namespace nm {
+    // MOD is exptected to be modular data type (int32_m, int32_n, int32_p)
+    template <std::int64_t M, std::int64_t P, typename MOD>
+    class ModHash{
+        private:
+            std::size_t n;
+            std::vector<MOD> hash;
+            std::vector<MOD> power;
+
+        public:
+            ModHash(std::string &s);
+            std::int64_t Interval(std::size_t left, std::size_t right);
+            ~ModHash() {};
+    };
+
+} // string hash
+
+namespace nm {
+    template <typename T>
+    class CoordinateCompression {
+        private:
+            Splay<T, T, Node<T> > coordinates;
+            std::vector<T> elements;
+        public:
+            CoordinateCompression(std::vector<T> data);
+            std::int32_t coordinate(T x);
+            T element(std::size_t i);
+            ~CoordinateCompression() {};
+    };
+} // coordinate compression
 
 #endif // HASH
--- src/hash.cpp
+++ src/hash.cpp
@@ -1,5 +1,7 @@
 #include <hash.hpp>
 
+#include <sort.hpp>
+
 namespace nm {
     template<std::int64_t M, std::int64_t P>
     Hash<M, P>::Hash(std::string &s) : Arithmetic<std::int64_t>::Arithmetic(M) {
@@ -23,3 +25,53 @@ namespace nm {
                 this->power[right - left]));
     }
 } // string
+
+namespace nm {
+    template<std::int64_t M, std::int64_t P, typename MOD>
+    ModHash<M, P, MOD>::ModHash(std::string &s) {
+        this->n = s.length();
+        this->hash.reserve(this->n + 1);
+        this->power.reserve(this->n + 1);
+
+        this->hash = {0};
+        this->power = {1};
+        for (char s_i : s) {
+            this->hash.push_back(this->add(this->multiply(this->hash.back(), P), s_i));
+            this->power.push_back(this->multiply(this->power.back(), P));
+        }
+    }
+
+    template<std::int64_t M, std::int64_t P, typename MOD>
+    std::int64_t ModHash<M, P, MOD>::Interval(std::size_t left, std::size_t right) {
+        assert(right <= this->n and right > left);
+        return this->hash[right] - this->hash[left] * this->power[right - left];
+    }
+} // hash function int32_m
+
+
+namespace nm {
+    template <typename T>
+    CoordinateCompression<T>::CoordinateCompression(std::vector<T> data) {
+        std::int32_t lo = 0; std::int32_t hi = data.size() - 1;
+        hybrid_sort<T>(0, hi, data);
+
+        for (T d : data) {
+            if (this->coordinates.search(d)) continue;
+            this->coordinates.insert(d, elements.size());
+            elements.push_back(d);
+        };
+    }
+
+    template <typename T>
+    std::int32_t CoordinateCompression<T>::coordinate(T x) {
+        if (not this->coordinates.search(x)) return -1;
+        return coordinates.access(x);
+    }
+
+    template <typename T>
+    T element(std::size_t i) {
+        assert(i < this->element.size())
+        return this->element[i];
+    }
+}
+
